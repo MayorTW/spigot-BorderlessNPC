816111ddaef5a0e2c4039d0e8a937b90ae358300
Rio6 <rio6.liu@gmail.com>
2017-07-08 14:36:56 -0700

    Improve sound horse and rider
    Add arm animation for sound horse, and let the rider face where the
    horse is facing

--- src/tw/mayortw/blenemies/trait/SoundHorseTrait.java
+++ src/tw/mayortw/blenemies/trait/SoundHorseTrait.java
@@ -1,7 +1,14 @@
 package tw.mayortw.blenemies.trait;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.ai.GoalController;
+import net.citizensnpcs.api.event.NPCDamageEntityEvent;
+import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.trait.Trait;
+import net.citizensnpcs.util.PlayerAnimation;
 import tw.mayortw.blenemies.goal.RandomStrollGoal;
 import tw.mayortw.blenemies.goal.TargetNearbyPlayerGoal;
 
@@ -26,7 +33,26 @@ public class SoundHorseTrait extends Trait {
         if(getNPC().isSpawned()) {
             if(getNPC().getEntity().getPassengers().size() == 0) {
                 getNPC().despawn();
+            } else {
+                for(Entity entity : getNPC().getEntity().getPassengers()) {
+                    entity.getLocation().setDirection(getNPC().getEntity().getLocation().getDirection());
+                    NPC rider = CitizensAPI.getNPCRegistry().getNPC(entity);
+                    if(rider != null) {
+                        rider.faceLocation(getNPC().getEntity().getLocation()
+                                .clone().add(getNPC().getEntity().getLocation().getDirection().normalize()));
+                    }
+                }
             }
+        }
+    }
+
+    @EventHandler
+    public void onNPCDamageEntity(NPCDamageEntityEvent eve) {
+        if(eve.getNPC().equals(this.getNPC())) {
+            Entity rider = eve.getNPC().getEntity().getPassengers().get(0);
+
+            if(rider instanceof Player)
+                PlayerAnimation.ARM_SWING.play((Player) rider);
         }
     }
 }

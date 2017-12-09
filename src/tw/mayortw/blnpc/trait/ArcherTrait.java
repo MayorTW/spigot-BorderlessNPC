7dba34760ab2bf0231382fbb5a1a9a64a4f042c1
Rio6 <rio6.liu@gmail.com>
2017-12-08 21:39:05 -0800

    Use custom stuck checker on ArchTrait, too

--- src/tw/mayortw/blnpc/trait/ArcherTrait.java
+++ src/tw/mayortw/blnpc/trait/ArcherTrait.java
@@ -5,7 +5,10 @@ import org.bukkit.Location;
 import org.bukkit.Sound;
 
 import net.citizensnpcs.api.ai.GoalController;
+import net.citizensnpcs.api.ai.Navigator;
 import net.citizensnpcs.api.ai.NavigatorParameters;
+import net.citizensnpcs.api.ai.StuckAction;
+import net.citizensnpcs.api.ai.event.NavigationStuckEvent;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.trait.LookClose;
@@ -18,6 +21,7 @@ import tw.mayortw.blnpc.goal.ShootTargetGoal;
 
 public class ArcherTrait extends Trait {
 
+    private int lastX, lastZ, stationaryTicks; // Edited from net.citizensnpcs.npc.ai.CitizensNavigator
     private Random random = new Random();
     private int soundCountdown;
     private Sound[] sounds = {  Sound.ENTITY_VILLAGER_AMBIENT,
@@ -47,11 +51,51 @@ public class ArcherTrait extends Trait {
 
     @Override
     public void run() {
+        checkStationaryStatus();
+
         if(npc.isSpawned() && soundCountdown <= 0) {
             // Make some noise
             npc.getEntity().getWorld().playSound(npc.getEntity().getLocation(),
                     sounds[random.nextInt(sounds.length)], 1, 1);
             soundCountdown = 100 + random.nextInt(100);
         } else soundCountdown--;
+    }
+
+    // Edited from net.citizensnpcs.npc.ai.CitizensNavigator
+
+    /*
+     * The stuck action will not be called by Citizens API if NPC is jumping
+     * so I handle it myself
+     */
+
+    private void checkStationaryStatus() {
+        if(!npc.isSpawned()) return;
+
+        Navigator navigator = npc.getNavigator();
+
+        if(navigator.isNavigating()) {
+            NavigatorParameters localParams = navigator.getLocalParameters();
+            if (localParams.stationaryTicks() < 0)
+                return;
+            Location current = npc.getEntity().getLocation();
+            if (lastX == current.getBlockX() && lastZ == current.getBlockZ()) {
+                if (++stationaryTicks >= localParams.stationaryTicks()) {
+                    StuckAction action = localParams.stuckAction();
+                    NavigationStuckEvent event = new NavigationStuckEvent(navigator, action);
+                    Bukkit.getPluginManager().callEvent(event);
+                    action = event.getAction();
+                    boolean shouldContinue = action != null ? action.run(npc, navigator) : false;
+                    if (shouldContinue) {
+                        stationaryTicks = 0;
+                    } else {
+                        navigator.cancelNavigation();
+                    }
+                    return;
+                }
+            } else
+                stationaryTicks = 0;
+            lastX = current.getBlockX();
+            lastZ = current.getBlockZ();
+        }
     }
 }

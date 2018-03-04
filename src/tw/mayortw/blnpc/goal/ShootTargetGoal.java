5107700cd61f2ddad791458550b213ef56e3cf3e
Rio6 <rio6.liu@gmail.com>
2018-03-04 12:29:18 -0800

    Add a bit random into archer's aiming

--- src/tw/mayortw/blnpc/goal/ShootTargetGoal.java
+++ src/tw/mayortw/blnpc/goal/ShootTargetGoal.java
@@ -112,7 +112,8 @@ public class ShootTargetGoal extends BehaviorGoalAdapter {
             double z = tgtLoc.getZ() - shtLoc.getZ();
 
             shooter.launchProjectile(org.bukkit.entity.Arrow.class,
-                    new Vector(x, y + tgtLoc.distance(shtLoc) * 0.20000000298023224D, z).normalize().multiply(1.6))
+                    new Vector(x + Math.random() - .5, y + tgtLoc.distance(shtLoc) * 0.20000000298023224D + Math.random() - .5, z + Math.random() - .5)
+                    .normalize().multiply(1.6))
                 .setPickupStatus(Arrow.PickupStatus.DISALLOWED);
 
             shootCoolDown = SHOOT_CD;
